d71168eb2e55b232a690fd976d46b957391f6c84
Kyle <Kyle.rogers533@gmail.com>
2016-02-14 22:13:40 -0600

    Adding code to system-register to handle module registration

--- src/system-register.ts
+++ src/system-register.ts
@@ -4,8 +4,16 @@ class SystemRegister {
   constructor(cache: ModuleCache) {}
 
   register(name: string, deps: string[], program: Function): void {
-    var module: any = {};
+    var module: any = {}; //blank object for module
+    var moduleModify: Function = function(prop: string, export: any): void {}
+
     var exports: Function = function(name: string, member: any) => module[name]       = member;
     var obj: any = program(exports);
+
+    for(var i in deps) {
+      // Call the setter for each dep and load into module object
+    }
+    // execute(obj) 
+    // Store the module in cache
   }
 }
