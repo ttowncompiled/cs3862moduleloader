02c7c02ab27fe5625f0e569307de72146ece5f83
josephedmunds <jee431@utulsa.edu>
2016-03-21 13:53:19 -0500

    Revisions to the module cache

--- module-cache.ts
+++ module-cache.ts
@@ -1,6 +1,11 @@
 import {Browser} from './browser';
+
 export class ModuleCache {
-    constructor(browser: Browser) { var dictionary = {}; }
-    get(names: string[]) => dictionary[names];
-    store(name: string, module: any): dictionary[name] = module;
+	cache: any = {};
+
+    constructor(public browser: Browser) {}
+
+    get(names: string[]): any {};
+
+    store(name: string, module: any): void => cache[name] = module;
 }
